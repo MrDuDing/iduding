918b2587e2e3a0773d91af67f38b89c9724ae09f
MrDuDing <673694553@qq.com>
2018-12-12 17:23:47 +0800

    Initial commit

---
+++ Application/Home/Controller/VueController.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class VueController extends Controller {
+    public function vue(){
+        $this->display();
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# iduding
